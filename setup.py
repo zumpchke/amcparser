56f59026e9742ec8cb656f6b6bb7f727ac9073e4
Vanush Vaswani <vanush@gmail.com>
2016-10-20 20:07:14 +1100

    add basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='amcparser',
+      version='0.1',
+      description='Get data from and view asf/amc files',
+      author='Vanush Vaswani',
+      author_email='vanush@gmail.com',
+      packages=find_packages(),
+      install_requires=['numpy', 'cgkit', 'asciitree', 'tqdm'],
+      )
